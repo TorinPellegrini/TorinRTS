96f3626fbfb503e627a1bba73d90543886d26f4d
TorinPellegrini <2102892@uad.ac.uk>
2023-11-06 14:05:54 +0000

    Initial commit

---
+++ Source/TorinRTS/TorinRTS.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TorinRTS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TorinRTS, "TorinRTS" );
+
+DEFINE_LOG_CATEGORY(LogTorinRTS)
+ 
